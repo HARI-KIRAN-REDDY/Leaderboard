7f4cc615eba075be10c9642656526c31842a64cf
HARI-KIRAN-REDDY <lokeshreddydevireddy@gmail.com>
2023-10-14 21:52:56 +0530

    Added leader board

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+const leaderboardData = {
+    "Sampath": { "match_predicton_score": 30, "most_runs_predicton_score": 8, "most_wickets_prediction_score": 16 },
+    "Yeswanth": { "match_predicton_score": 35, "most_runs_predicton_score": 8, "most_wickets_prediction_score": 10 },
+    "Charan": { "match_predicton_score": 25, "most_runs_predicton_score": 13, "most_wickets_prediction_score": 15 },
+    "Neeraj": { "match_predicton_score": 25, "most_runs_predicton_score": 13, "most_wickets_prediction_score": 13 },
+    "Manoj": { "match_predicton_score": 25, "most_runs_predicton_score": 13, "most_wickets_prediction_score": 13 },
+    "Laksman": { "match_predicton_score": 30, "most_runs_predicton_score": 8, "most_wickets_prediction_score": 13 },
+    "Durga Raj": { "match_predicton_score": 25, "most_runs_predicton_score": 5, "most_wickets_prediction_score": 13 },
+    "ManiKanta": { "match_predicton_score": 30, "most_runs_predicton_score": 3, "most_wickets_prediction_score": 8 },
+    "Sathvik": { "match_predicton_score": 10, "most_runs_predicton_score": 3, "most_wickets_prediction_score": 5 }
+};
+
+// Sort leaderboardData based on total score in descending order
+const sortedLeaderboardData = Object.entries(leaderboardData)
+    .sort(([, a], [, b]) => (b.match_predicton_score + b.most_runs_predicton_score + b.most_wickets_prediction_score) - (a.match_predicton_score + a.most_runs_predicton_score + a.most_wickets_prediction_score))
+    .reduce((acc, [key, value]) => ({ ...acc, [key]: value }), {});
+
+console.log(sortedLeaderboardData);
+
+    // Calculate total score and sort the leaderboard
+    const leaderboardArray = Object.entries(leaderboardData).map(([player, scores]) => {
+        const totalScore = scores.match_predicton_score + scores.most_runs_predicton_score + scores.most_wickets_prediction_score;
+        return { player, ...scores, totalScore };
+    }).sort((a, b) => b.totalScore - a.totalScore);
+
+    // Populate the leaderboard table
+    const tableBody = document.querySelector("#leaderboard-table tbody");
+    leaderboardArray.forEach(entry => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${entry.player}</td>
+            <td>${entry.match_predicton_score}</td>
+            <td>${entry.most_runs_predicton_score}</td>
+            <td>${entry.most_wickets_prediction_score}</td>
+            <td>${entry.totalScore}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+});
